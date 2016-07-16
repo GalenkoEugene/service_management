25e4cfc7932fdcaf1bce4bc8a4d4bed1818b0781
Eugen <Re4port@ukr.net>
2016-07-16 09:56:10 +0300

    added dynamic title, used helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Service"
+    full_base_title = "Service Management"
+    if page_title.empty?
+      full_base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
