d485537cfe202888a0606709c884ac0bf6a74410
Eugen <Re4port@ukr.net>
2016-07-21 18:20:58 +0300

    successfully install rspec and made first working test

--- spec/views/repair_orders/orders.html.erb_spec.rb
+++ spec/views/repair_orders/orders.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "repair_orders/orders.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe "repair_orders/orders.html.erb", type: :view do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+describe "RepairOrders" do
+
+  describe "orders" do
+
+    it "should have the content 'RepairOrders#orders'" do
+      visit '/repair_orders/orders'
+      expect(page).to have_content('RepairOrders#orders')
+    end
+  end
 end
